76acc21bb2f799172a7aafeea5b9912065177b97
Reinaldo J. Nunes <reinaldo.nunes2@gmail.com>
2021-10-10 17:11:58 -0300

    Finalização da interface gráfica da página

--- assets/api/apiconfig.js
+++ assets/api/apiconfig.js
@@ -3,14 +3,14 @@ let baseUrl = 'https://api.nasa.gov';
 let api_key ='Y1NJVaV0VWcI9AnYNkcmTMdhLpcWMwlRjqpbIyW3';
 
 var heroimage = document.createElement('script');
-heroimage.src = 'api/herodaily.js';
+heroimage.src = 'assets/api/herodaily.js';
 document.body.appendChild(heroimage);
 
 
 var asteroids = document.createElement('script');
-asteroids.src = 'api/asteroidapi.js';
+asteroids.src = 'assets/api/asteroidapi.js';
 document.body.appendChild(asteroids);
 
 var marsgallery = document.createElement('script');
-marsgallery.src = 'api/marsgalleryapi.js';
+marsgallery.src = 'assets/api/marsgalleryapi.js';
 document.body.appendChild(marsgallery);
